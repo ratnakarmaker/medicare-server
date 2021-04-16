250633e51a8392b4809cec52f28a4500482e9ba8
ratnakarmaker <73543192+ratnakarmaker@users.noreply.github.com>
2021-04-16 19:24:05 +0600

    add

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const fileupload = require("express-fileupload");
 const MongoClient = require("mongodb").MongoClient;
 const ObjectId = require("mongodb").ObjectID;
 
-const PORT = process.env.PORT || 5000;
+const port = 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -16,10 +16,6 @@ app.use(fileupload());
 
 app.get("/", (req, res) => {
   res.send("Welcome Backhand");
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running ${"http://localhost:5000/"}`);
 });
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vdydn.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
@@ -142,3 +138,5 @@ client.connect((err) => {
       });
   });
 });
+
+app.listen(process.env.PORT || port)
